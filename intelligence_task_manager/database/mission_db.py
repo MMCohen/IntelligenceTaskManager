ec3784f3230110d12239bc87339da465d1aa2f95
Mendel <MendelCohen770@gmail.com>
2026-06-17 16:48:41 +0300

    add create_mission func

--- intelligence_task_manager/database/mission_db.py
+++ intelligence_task_manager/database/mission_db.py
@@ -3,10 +3,56 @@ from intelligence_task_manager.database.connection_db import ConnectionDB
 connection = ConnectionDB()
 
 class MissionDB:
-    pass
 
-    def create_mission(self, data):
-        pass
+    def create_mission(self, data: dict) -> dict:
+        """
+        add new mission to the database
+        :param data:
+        :return:
+        """
+        connector = connection.get_connection()
+        cursor = connector.cursor(dictionary=True)
+
+        # sql_fields = ", ".join([f"{key}" for key in data.keys()])
+        # sql_place_holders = "%s, " * len(data)
+        # sql_vals = list(data.values())
+
+        risk_level = self.calculate_risk_level(data["difficulty"], data["importance"])
+
+        sql_vals = data["title"], data["description"], data["location"], data["difficulty"], data["importance"], risk_level
+
+        try:
+            cursor.execute(f"""
+            INSERT INTO missions (title, description, location, difficulty, importance, risk_level)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ;
+            """, sql_vals)
+            last_id = cursor.lastrowid
+            connector.commit()
+
+            cursor.execute("SELECT * FROM missions WHERE id = %s;", (last_id, ))
+            new_mission = cursor.fetchone()
+            return new_mission
+
+        finally:
+            cursor.close()
+            connector.close()
+
+
+    def calculate_risk_level(self, difficulty: int, importance: int) -> str | None:
+        """
+        calculate risk level using the formula (difficulty * 2) + importance
+        """
+        risk_level = (difficulty * 2) + importance
+
+        if 1 <= risk_level <= 9:
+            return "LOW"
+        elif 10 <= risk_level <= 17:
+            return "MEDIUM"
+        elif 18 <= risk_level <= 24:
+            return "HIGH"
+        elif 25 <= risk_level:
+            return "CRITICAL"
 
 
     def get_all_missions(self) -> list[dict | None]:
@@ -79,5 +125,7 @@ class MissionDB:
 
 if __name__ == "__main__":
     mis = MissionDB()
-    print(mis.get_all_missions())
+    new_mis = {"title": "title", "description": "dfsdf" , "location": "dfsdf", "difficulty": 6, "importance": 6, "risk_level": "low"}
+    print(mis.create_mission(new_mis))
+    # print(mis.get_all_missions())
 
