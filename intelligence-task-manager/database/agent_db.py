e68fc29d55c27a3c8b8688d37e203a4410eff0fc
Mendel <MendelCohen770@gmail.com>
2026-06-17 16:06:46 +0300

    add classes structure

---
+++ intelligence-task-manager/database/agent_db.py
@@ -0,0 +1,43 @@
+
+
+class AgentDB:
+
+    def create_agent(self, data):
+        pass
+
+
+    def get_all_agents(self):
+        pass
+
+
+    def get_agent_by_id(self, id):
+        pass
+
+
+    def update_agent(self, id, data):
+        pass
+
+
+    def update_agent(self, id, data):
+        pass
+
+
+    def deactivate_agent(self, id):
+        pass
+
+
+    def increment_completed(self, id):
+        pass
+
+
+    def increment_failed(self, id):
+        pass
+
+
+    def get_agent_performance(self, id):
+        pass
+
+
+    def agents_active_count(self):
+        pass
+
